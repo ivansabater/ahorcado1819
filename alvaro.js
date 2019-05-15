e9db238ea403fd73f9ec9e8f089d9523c393ad74
Alvaropalo <alvaropalomaresgomez@gmail.com>
2019-05-15 12:02:27 +0200

    Imagenes  funcion

--- alvaro.js
+++ alvaro.js
@@ -1,7 +1,22 @@
- 
-    /*function imagen(){
-        let i = 0
-            if()
-    
+ /*
+    function imagen(numerrores){
+        switch (numerrores) {
+            case 0:
+            return "0.svg";
+            case 1:
+            return "1.svg";
+            case 2:
+            return "2.svg";
+            case 3:
+            return "3.svg";
+            case 4:
+            return "4.svg";
+            case 5:
+            return "5.svg";
+            case 6:
+            return "6.svg";    
+            default:
+                break;
         }
-    */
+    }
+  */  
